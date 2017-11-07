8a419bd6e6f008c1a14e1eb7a2f2b2901cdb0b1b
Rafael Pintar Alevato <rpa.alevato@gmail.com>
2017-11-07 19:44:29 -0200

    Added the correct files and started to work on them

--- control/futureTimeDate/dateTime.h
+++ control/futureTimeDate/dateTime.h
@@ -1,6 +1,23 @@
+/** 
+ *	@author  Rafael Pintar Alevato
+ *	@date    07/11/2017
+ *	@version 0.1 
+ *	
+ *	@brief Date and time tracking.
+ *
+ *	@section DESCRIPTION
+ *	
+ *	Creates a class that has a date (DD/MM/YYYY) and a time (HH:MM:SS)
+ *	It can get current time and change its attributes. It does corrections for
+ *	Brazil daylight saving time and leap years.
+ */
+
 #ifndef DATETIME
 #define DATETIME
 
+#include <iostream>
+
+using namespace std;
 
 
 #endif	// DATETIME
