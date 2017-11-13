85743e45c5e152ec568ac4f54e51f2f779066a0d
Rafael Pintar Alevato <rpa.alevato@gmail.com>
2017-11-13 00:25:37 -0200

    Forgot this file.

--- control/controlInterface.cpp
+++ control/controlInterface.cpp
@@ -1,2 +1,41 @@
 #include "controlInterface.h"
 
+ControlInterface::ControlInterface() {
+	
+}
+
+ControlInterface::~ControlInterface() {
+	
+}
+
+void ControlInterface::checkin(User currentUser) {
+	
+}
+
+void ControlInterface::checkout(User currentUser) {
+	
+}
+
+SortedList ControlInterface::getActiveUsers() {
+	
+}
+
+void ControlInterface::openDoor(User currentUser) {
+	
+}
+
+void ControlInterface::createUser(User inputUser) {
+	
+}
+
+void ControlInterface::removeUser(unsigned long int id) {
+	
+}
+
+void ControlInterface::editUser(User inputUser) {
+	
+}
+
+User ControlInterface::getUser(unsigned long int id) {
+	
+}
